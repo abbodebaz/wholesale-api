68770a25ad5279c3d715543ca362bdde527a09ef
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2025-11-23 14:58:29 +0300

    Update login.php

--- auth/login.php
+++ auth/login.php
@@ -1,4 +1,15 @@
 <?php
+
+header("Access-Control-Allow-Origin: https://ebaaptl.com");
+header("Access-Control-Allow-Origin: https://www.ebaaptl.com");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 header("Content-Type: application/json");
 require_once "../config.php";
 
