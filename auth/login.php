4a455dcf28417905549efb5eab0765aed2e88d8b
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2025-11-23 15:01:51 +0300

    Refactor CORS handling for allowed origins

--- auth/login.php
+++ auth/login.php
@@ -1,7 +1,14 @@
 <?php
 
-header("Access-Control-Allow-Origin: https://ebaaptl.com");
-header("Access-Control-Allow-Origin: https://www.ebaaptl.com");
+$allowedOrigins = [
+    "https://ebaaptl.com",
+    "https://www.ebaaptl.com"
+];
+
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+}
+
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 
@@ -9,6 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
 }
+
 
 header("Content-Type: application/json");
 require_once "../config.php";
