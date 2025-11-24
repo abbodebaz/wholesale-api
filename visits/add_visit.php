63ba94a2721731a16a3730b7ed244a81c28379f8
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2025-11-24 12:52:12 +0300

    Add CORS support to add_visit.php

--- visits/add_visit.php
+++ visits/add_visit.php
@@ -1,5 +1,22 @@
 <?php
 
+$allowedOrigins = [
+    "https://ebaaptl.com",
+    "https://www.ebaaptl.com"
+];
+
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+}
+
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 header("Content-Type: application/json");
 require_once "../config.php";
 
